ca497b92f0cf03b377874eaaaa7f225b8f730b1b
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-10-28 22:17:39 -0700

    test: update tests for dynamic solver

--- tests/test_dz3.py
+++ tests/test_dz3.py
@@ -2,27 +2,23 @@ from problem_space.dz3 import dynamicSolver
 
 
 def test_single_variable_single_answer():
-    solution = list(dynamicSolver(["x = Int('x')"], ["x == 1"]))
+    solution = list(dynamicSolver({"x": "Int"}, ["x == 1"]))
     assert solution == [{"x": 1}]
 
 
 def test_single_variable_multiple_answers():
-    solution = list(dynamicSolver(["x = Int('x')"], ["x > 0", "x < 3"]))
+    solution = list(dynamicSolver({"x": "Int"}, ["x > 0", "x < 3"]))
     assert solution == [{"x": 1}, {"x": 2}]
 
 
 def test_multiple_variables_single_answer():
-    solution = list(
-        dynamicSolver(["x = Int('x')", "y = Int('y')"], ["x == 0", "y == 1"])
-    )
+    solution = list(dynamicSolver({"x": "Int", "y": "Int"}, ["x == 0", "y == 1"]))
     assert solution == [{"x": 0, "y": 1}]
 
 
 def test_multiple_variables_multiple_answers():
     solution = list(
-        dynamicSolver(
-            ["x = Int('x')", "y = Int('y')"], ["x > 0", "x < 3", "y > 0", "y < 3"]
-        )
+        dynamicSolver({"x": "Int", "y": "Int"}, ["x > 0", "x < 3", "y > 0", "y < 3"])
     )
     assert solution == [
         {"x": 1, "y": 1},
